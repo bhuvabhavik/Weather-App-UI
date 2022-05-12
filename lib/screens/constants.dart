a9dd7508a5c7e357408111e12d34e33a0dc4fd9e
bhavik bhuva <BhavikBhuva80@gmail.com>
2022-05-12 18:26:41 +0530

    slider design

---
+++ lib/screens/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const tstyle14 = TextStyle(color: Colors.white, fontSize: 14);
+const tstyle16 = TextStyle(color: Colors.white, fontSize: 16);
+const tstyle20 = TextStyle(color: Colors.white, fontSize: 20);
--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/widgets.dart';
+import 'single_weather.dart';
 
 void main() {
   runApp(WeatherUI());
 }
-
-const tstyle14 = TextStyle(color: Colors.white, fontSize: 14);
-const tstyle16 = TextStyle(color: Colors.white, fontSize: 16);
-const tstyle20 = TextStyle(color: Colors.white, fontSize: 20);
 
 class WeatherUI extends StatelessWidget {
   const WeatherUI({Key? key}) : super(key: key);
@@ -59,177 +57,68 @@ class WeatherUI extends StatelessWidget {
               color: Color(0xa1201c1c),
             ), //opacity controller
             Container(
-              padding: EdgeInsets.all(20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
+              margin: EdgeInsets.only(left: 15, top: 140),
+              child: Row(
                 children: [
-                  Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(
-                              height: 150,
-                            ),
-                            Text(
-                              "Toronto Canada",
-                              style: GoogleFonts.lato(
-                                  fontSize: 30,
-                                  fontWeight: FontWeight.bold,
-                                  color: Colors.white),
-                            ),
-                            SizedBox(
-                              height: 6,
-                            ),
-                            Text(
-                              '07:15 PM - THURSDAY, 12 MAY 2022',
-                              style: GoogleFonts.lato(
-                                  color: Colors.white,
-                                  fontSize: 14,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "5\u2103",
-                              style: GoogleFonts.lato(
-                                  fontSize: 85,
-                                  fontWeight: FontWeight.w300,
-                                  color: Colors.white),
-                            ),
-                            Row(
-                              children: [
-                                Text(
-                                  '🌜',
-                                  style: tstyle20,
-                                ),
-                                SizedBox(
-                                  width: 8,
-                                ),
-                                Text(
-                                  'Night',
-                                  style: GoogleFonts.lato(
-                                      color: Colors.white, fontSize: 18),
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                      ],
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 5),
+                    width: 12,
+                    height: 5,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8),
+                      ),
                     ),
                   ),
-                  Column(
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 40),
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.white38)),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 5),
+                    width: 5,
+                    height: 5,
+                    decoration: BoxDecoration(
+                      color: Colors.white54,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8),
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Column(
-                              children: [
-                                Text(
-                                  'Wind',
-                                  style: tstyle14,
-                                ),
-                                Text(
-                                  '10',
-                                  style: tstyle16,
-                                ),
-                                Text(
-                                  'km/p',
-                                  style: tstyle14,
-                                ),
-                                Stack(
-                                  children: [
-                                    Container(
-                                      width: 50,
-                                      height: 5,
-                                      color: Colors.white38,
-                                    ),
-                                    Container(
-                                      width: 15,
-                                      height: 5,
-                                      color: Colors.greenAccent,
-                                    )
-                                  ],
-                                )
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Text('Rain', style: tstyle14),
-                                Text('89', style: tstyle16),
-                                Text('%', style: tstyle14),
-                                Stack(
-                                  children: [
-                                    Container(
-                                      height: 5,
-                                      width: 50,
-                                      color: Colors.white38,
-                                    ),
-                                    Container(
-                                      height: 5,
-                                      width: 5,
-                                      color: Colors.redAccent,
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
-                            Column(
-                              children: [
-                                Text('humidity', style: tstyle14),
-                                Text('75', style: tstyle16),
-                                Text('%', style: tstyle14),
-                                Stack(
-                                  children: [
-                                    Container(
-                                      height: 5,
-                                      width: 50,
-                                      color: Colors.white38,
-                                    ),
-                                    Container(
-                                      height: 5,
-                                      width: 40,
-                                      color: Colors.greenAccent,
-                                    )
-                                  ],
-                                )
-                              ],
-                            ),
-                            // Text(
-                            //   "Bhuj Gujarat",
-                            //   style: GoogleFonts.lato(
-                            //       fontSize: 35,
-                            //       fontWeight: FontWeight.bold,
-                            //       color: Colors.white),
-                            // ),
-                            // Text(
-                            //   'hello',
-                            //   style: GoogleFonts.lato(
-                            //       color: Colors.white, fontSize: 18),
-                            // ),
-                          ],
-                        ),
-                      )
-                    ],
-                  )
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 5),
+                    width: 5,
+                    height: 5,
+                    decoration: BoxDecoration(
+                      color: Colors.white54,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 5),
+                    width: 5,
+                    height: 5,
+                    decoration: BoxDecoration(
+                      color: Colors.white54,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 5),
+                    width: 5,
+                    height: 5,
+                    decoration: BoxDecoration(
+                      color: Colors.white54,
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             ),
+            SingleWeather(),
           ],
         ),
       ),
