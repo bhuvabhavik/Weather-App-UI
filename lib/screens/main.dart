73fa28946f0bd3335eb20a1ce928d463987ae265
bhavik bhuva <BhavikBhuva80@gmail.com>
2022-05-12 16:31:23 +0530

    structured positions of the rows and colums of the app

--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(WeatherUI());
@@ -16,19 +17,25 @@ class WeatherUI extends StatelessWidget {
         appBar: AppBar(
           backgroundColor: Colors.transparent,
           elevation: 0,
-          leading: Icon(
-            Icons.search,
-            color: Colors.white,
-            size: 35,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.search,
+              color: Colors.white,
+              size: 30,
+            ),
           ),
           actions: [
             Container(
               margin: EdgeInsets.only(right: 20),
-              child: SvgPicture.asset(
-                'assets/images/menu.svg',
-                height: 35,
-                width: 35,
-                color: Colors.white,
+              child: GestureDetector(
+                onTap: () {},
+                child: SvgPicture.asset(
+                  'assets/images/menu.svg',
+                  height: 30,
+                  width: 30,
+                  color: Colors.white,
+                ),
               ),
             )
           ],
@@ -45,8 +52,88 @@ class WeatherUI extends StatelessWidget {
             Container(
               height: double.infinity,
               width: double.infinity,
-              color: Color(0xa2201c1c),
-            )
+              color: Color(0xa1201c1c),
+            ), //opacity controller
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 120,
+                            ),
+                            Text(
+                              "Bhuj Gujarat",
+                              style: GoogleFonts.lato(
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                            Text(
+                              'hello',
+                              style: GoogleFonts.lato(
+                                  color: Colors.white, fontSize: 18),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Bhuj Gujarat",
+                              style: GoogleFonts.lato(
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                            Text(
+                              'hello',
+                              style: GoogleFonts.lato(
+                                  color: Colors.white, fontSize: 18),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 40),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white)),
+                      ),
+                      Row(
+                        children: [
+                          Text(
+                            "Bhuj Gujarat",
+                            style: GoogleFonts.lato(
+                                fontSize: 35,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            'hello',
+                            style: GoogleFonts.lato(
+                                color: Colors.white, fontSize: 18),
+                          ),
+                        ],
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
